cd9943845a24f78c9ef369d3da24f26c29623c0e
hughest64 <hughest64@gmail.com>
2016-04-05 16:22:48 -0500

    Update and rename main to main.rkt

--- main.rkt
+++ main.rkt
@@ -16,33 +16,36 @@
 (define SHP "radial-star")  ;shape of ballon
 (define PTS 10)             ;# of points on exploded ballon
 (define MIN-R 10)           ;minimum radius of ballon
-(define MAX-R 100)          ;maximum radius of ballon
+(define MAX-R 100)          ;maximum radius of ballon3
+(define SLD "solid")        ;image type
 
 ;; =================
 ;; Data definitions:
 
-(define-struct ballon (SHP PTS R1 R2 C))
-;; BALLOON is (make-ballon (String Natural Natural[MIN-R, MAX-R] Natural[MIN-R, MAX-R] String))
+(define-struct ballon (SHP PTS R1 R2 SLD C))
+;; BALLOON is (make-ballon (String Natural Natural[MIN-R, MAX-R] Natural[MIN-R, MAX-R] String String))
 ;; interp.  (make-ballon (SHP PTS R1 R2 C)) is a balloon with 
 ;;          SHP is the shape of the ballon (radial-star)
-;;          PTS is the number of points on the exploded balloon
+;;          PTS is the number of points on the exploded balloon (10)
 ;;          R1 is the inner diamter
 ;;          R2 is the outter diameter
+;;          SLD is a solid shape
 ;;          C is the color of the balloon                              ;THIS WILL NEED TO BE AN ENUMERATION !!!
-(define B1 (make-balloon (SHP PTS MIN MIN "red")))
-(define B2 (make-balloon (SHP PTS MAX MAX "blue")))
-(define B3 (make-balloon (SHP PTS 20  20  "green")))
-(define B4 (make-balloon (SHP PTS MAX MIN "purple")))
+(define B1 (make-balloon (SHP PTS MIN MIN SLD "red")))
+(define B2 (make-balloon (SHP PTS MAX MAX SLD "blue")))
+(define B3 (make-balloon (SHP PTS 20  20  SLD "green")))
+(define B4 (make-balloon (SHP PTS MAX MIN SLD "purple")))
 
 (define (fn-for-baloon B)
-  (... (ballon-SHP B)   ;String
-       (ballon-PTS B)   ;Natural
-       (ballon-R1 B)    ;Natural[MIN, MAX]
-       (ballon-R2 B)    ;Natural[MIN, MAX]
-       (ballon-C B)))   ;String
+  (... (balloon-SHP B)   ;String
+       (balloon-PTS B)   ;Natural
+       (balloon-R1 B)    ;Natural[MIN, MAX]
+       (balloon-R2 B)    ;Natural[MIN, MAX]
+       (balloon-SLD)     ;String
+       (balloon-C B)))   ;String
 
 ;; Template rules used:
-;;  - Compound 5 cases
+;;  - Compound 6 cases
 
 ;; =================
 ;; Functions:
