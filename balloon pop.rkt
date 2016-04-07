fbdb9c5b7c2fe23db3cda2400fdee39decc8ca4c
hughest64 <hughest64@gmail.com>
2016-04-06 21:41:27 -0500

    Added files via upload
    updated world. Program runs! Needs refining of render function and to add on mouse and on key functions

--- balloon pop.rkt
+++ balloon pop.rkt
@@ -16,7 +16,7 @@
 
 (define MTS (empty-scene WIDTH HEIGHT))
 
-(define SHP "radial-star")  ;shape of balloon
+;(define SHP "radial-star")  ;shape of balloon
 (define PTS 10)             ;# of points on exploded balloon
 (define MIN-R 10)           ;minimum radius of balloon
 (define MAX-R 100)          ;maximum radius of balloon
@@ -25,19 +25,23 @@
 ;; =================
 ;; Data definitions:
 
-(define-struct balloon (SHP PTS R1 R2 SLD C))
+(define-struct balloon (PTS R1 R2 SLD C))
 ;; BALLOON is (make-balloon (String Natural Natural[MIN-R, MAX-R] Natural[MIN-R, MAX-R] String String))
 ;; interp.  (make-balloon (SHP PTS R1 R2 SLD C)) is a balloon with 
-;;          SHP is the shape of the balloon (radial-star)
+
 ;;          PTS is the number of points on the exploded balloon (10)
 ;;          R1 is the inner diamter
 ;;          R2 is the outer diameter
 ;;          SLD is a solid shape
-;;          C is the color of the balloon                              ;THIS WILL NEED TO BE AN ENUMERATION !!!
-(define B1 (make-balloon SHP PTS MIN-R MIN-R SLD "red"))
-(define B2 (make-balloon SHP PTS MAX-R MAX-R SLD "blue"))
-(define B3 (make-balloon SHP PTS 20    20    SLD "green"))
-(define B4 (make-balloon SHP PTS MAX-R MIN-R SLD "purple"))
+;;          C is of of:
+;;          - "red"
+;;          - "blue"
+;;          - "green"
+;;          - "purple"
+(define B1 (make-balloon PTS MIN-R MIN-R SLD "red"))
+(define B2 (make-balloon PTS MAX-R MAX-R SLD "blue"))
+(define B3 (make-balloon PTS 20    20    SLD "green"))
+(define B4 (make-balloon PTS MAX-R MIN-R SLD "purple"))
 #;
 (define (fn-for-baloon B)           ; !!!
   (... (balloon-SHP B)    ;String
@@ -46,6 +50,7 @@
        (balloon-R2  B)    ;Natural[MIN-R, MAX-R]
        (balloon-SLD B)    ;String
        (balloon-C   B)))  ;String
+
 
 ;; Template rules used:
 ;;  - Compound 6 cases
@@ -58,39 +63,39 @@
 ;; 
 (define (main B)
   (big-bang B                             ; BALLOON
-            (on-tick   next-balloon 1)    ; BALLOON -> BALLOON
+            (on-tick   next-balloon)      ; BALLOON -> BALLOON
             (to-draw   render-balloon)))  ; BALLOON -> Image
-;           (on-mouse  ...)      ; BALLOON Integer Integer MouseEvent -> BALLOON  ;leave out of the first build !!!
+;           (on-mouse  ...)      ; BALLOON Integer Integer MouseEvent -> BALLOON  ;leave out of the first build
             
 
 ;; BALLOON -> BALLOON
 ;; produce the next inner radius, outer radius, and color of BALLOON
-(check-expect (next-balloon (make-balloon SHP PTS MIN-R MIN-R SLD "red"))    (make-balloon SHP PTS 15    15    SLD "blue"))
-(check-expect (next-balloon (make-balloon SHP PTS 30    30    SLD "blue"))   (make-balloon SHP PTS 35    35    SLD "green"))
-(check-expect (next-balloon (make-balloon SHP PTS 50    50    SLD "green"))  (make-balloon SHP PTS 55    55    SLD "purple"))
+(check-expect (next-balloon (make-balloon PTS MIN-R MIN-R SLD "red"))    (make-balloon PTS 15    15    SLD "blue"))
+(check-expect (next-balloon (make-balloon PTS 30    30    SLD "blue"))   (make-balloon PTS 35    35    SLD "green"))
+(check-expect (next-balloon (make-balloon PTS 50    50    SLD "green"))  (make-balloon PTS 55    55    SLD "purple"))
 
-(check-expect (next-balloon (make-balloon SHP PTS MAX-R MAX-R SLD "purple")) (make-balloon SHP PTS MIN-R MAX-R SLD "red"))
+(check-expect (next-balloon (make-balloon PTS MAX-R MAX-R SLD "purple")) (make-balloon PTS MIN-R MAX-R SLD "red"))
 
 ;(define (next-balloon B) B)  ;stub
 
-;template is from BALLOON
+;template is from BALLOON !!!
 
-(define (next-balloon B)           
+(define (next-balloon B)           ; !!!
   (cond [(and (< (balloon-R1 B) MAX-R) (< (balloon-R2 B) MAX-R))
-         (make-balloon SHP PTS (+ 5 (balloon-R1 B)) (+ 5 (balloon-R2 B)) SLD (choose-color B))]
+         (make-balloon PTS (+ 5 (balloon-R1 B)) (+ 5 (balloon-R2 B)) SLD (choose-color B))]
         [else
-         (make-balloon SHP PTS MIN-R MAX-R SLD "red")]))
+         (make-balloon PTS MIN-R MAX-R SLD "red")]))
 
-;; BALLOON -> String
-;; choose the next color of balloon
-(check-expect (choose-color (make-balloon SHP PTS 40    40    SLD "red"))    "blue")
-(check-expect (choose-color (make-balloon SHP PTS 40    40    SLD "blue"))   "green")
-(check-expect (choose-color (make-balloon SHP PTS 33    33    SLD "green"))  "purple")
-(check-expect (choose-color (make-balloon SHP PTS 60    60    SLD "purple")) "red")
+;;; BALLOON -> String
+;;; choose the next color of balloon
+(check-expect (choose-color (make-balloon PTS 40    40    SLD "red"))    "blue")
+(check-expect (choose-color (make-balloon PTS 40    40    SLD "blue"))   "green")
+(check-expect (choose-color (make-balloon PTS 33    33    SLD "green"))  "purple")
+(check-expect (choose-color (make-balloon PTS 60    60    SLD "purple")) "red")
 
-;(define (choose-color B) "yellow") ;stub
+;;(define (choose-color B) "yellow") ;stub
 
-;template from BALLOON
+;;template from BALLOON
 
 (define (choose-color B)           
   (cond [(string=? (balloon-C B) "red") "blue"]
@@ -106,22 +111,27 @@
        
 ;; BALLOON -> Image
 ;; render the next balloon
-(check-expect (render-balloon  (make-ballon SHP PTS 50    50    SLD (choose-color B)))
-              (place-image     (make-ballon SHP PTS 55    55    SLD (choose-color B)) CTR-X CTR-Y MTS))
-(check-expect (render-balloon  (make-ballon SHP PTS MIN-R 15    SLD (choose-color B))) 
-              (place-image     (make-ballon SHP PTS 15    20    SLD (choose-color B)) CTR-X CTR-Y MTS))
+(check-expect (render-balloon  (make-balloon PTS 50    50 SLD "red"))              
+              (place-image     (radial-star  PTS 55    55 SLD "blue") CTR-X CTR-Y MTS))
+(check-expect (render-balloon  (make-balloon PTS MIN-R 15 SLD "blue")) 
+              (place-image     (radial-star  PTS 15    20 SLD "green") CTR-X CTR-Y MTS))
 
-(check-expect (render-balloon  (make-ballon SHP PTS MAX-R MAX-R SLD (choose-color B))) 
-              (place-image     (make-ballon SHP PTS MAX-R MIN-R SLD (choose-color B)) CTR-X CTR-Y MTS))
+(check-expect (render-balloon  (make-balloon PTS MAX-R MAX-R SLD "blue"))
+              (place-image     (radial-star  PTS MAX-R MIN-R SLD "red") CTR-X CTR-Y MTS))
 
-(define (render-balloon B) MTS)
+;(define (render-balloon B) MTS) 'stub
 
-#;
-(define (fn-for-baloon B)           ; !!!
+
+(define (render-balloon B)           ; !!!
   (cond [(and (< (balloon-R1 B) MAX-R) (< (balloon-R2 B) MAX-R))
-         (make-balloon SHP PTS (balloon-R1 B) (balloon-R2 B) SLD (choose-color B))]
+         (place-image (radial-star PTS (balloon-R1 B) (balloon-R2 B) SLD (choose-color B)) CTR-X CTR-Y MTS)]
         [else 
-         (make-balloon SHP PTS MAX-R          MIN-R          SLD "red")]))
+         (place-image (radial-star PTS MAX-R          MIN-R          SLD "red") CTR-X CTR-Y MTS)]))
+
+
+
+
+
 
 
 
