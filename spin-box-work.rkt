fcef03d11b674d9096aa9acc754545f05c134a98
hughest64 <hughest64@gmail.com>
2016-04-13 12:55:38 -0500

    Create spin-box-work.rkt
    working version of spinning box rackett project

---
+++ spin-box-work.rkt
@@ -0,0 +1,59 @@
+(define WIDTH 700)
+(define HEIGHT WIDTH)
+(define CTR-X (/ WIDTH 2))
+(define CTR-Y (/ HEIGHT 2))
+(define MTS (empty-scene WIDTH HEIGHT))
+
+
+(define GROW-RATE 1)
+(define ROTATE-RATE 15)
+
+(define-struct grapple (a s))
+
+;; Grapple -> Grapple
+;; produce then next size grapple at the next angle
+
+(check-expect (next-grapple  (make-grapple 0 0)) 
+              (make-grapple 1 15))
+(check-expect (next-grapple  (make-grapple 2 30)) 
+              (make-grapple (+ GROW-RATE 2) (+ ROTATE-RATE 30)))
+
+;(define (next-grapple b) b) ;stub
+
+;(define (fn-for-grapple b)  ;template
+; (... (grapple-a b)         ;natural
+;      (grapple-s b)))       ;natural
+
+
+(define (next-grapple b)
+  (cond [(and (number? (grapple-a b)) (number? (grapple-s b)))
+         (make-grapple (+ GROW-RATE (grapple-a b)) (+ ROTATE-RATE (grapple-s b)))]
+        [else b]))
+
+;; Grapple -> Image
+;; produe the correct image of grapple on MTS
+(check-expect (render-grapple (make-grapple 45 20)) 
+              (place-image (rotate 45 (square 20 "solid" "red")) CTR-Y CTR-X MTS))
+
+;(define (render-grapple b) (square 15 "solid" "blue")) ;stub
+
+(define (render-grapple b)  
+ (if (and (number? (grapple-a b)) (number? (grapple-s b))) 
+        (place-image (rotate (grapple-a b) (square (grapple-s b) "solid" "red")) 
+                     CTR-Y 
+                     CTR-X 
+                     MTS)
+     b))
+
+;; Grapple KeyEvent -> Grapple
+;; reset the program to 0 0 when space bar is pressed
+(check-expect (handle-key (make-grapple 12 60) " ") (make-grapple 0 0))
+(check-expect (handle-key (make-grapple 50 23) "a") (make-grapple 50 23))
+
+;(define (handle-key b ke) b) ;stub
+ 
+
+(define (handle-key b ke)  
+ (if (and (number? (grapple-a b)) (number? (grapple-s b)) (key=? ke " "))    
+      (make-grapple 0 0)
+      b))
