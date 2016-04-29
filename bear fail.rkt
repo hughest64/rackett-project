efa514c52e1283ccc87cd9435727362fcff74fc8
hughest64 <hughest64@gmail.com>
2016-04-29 12:23:57 -0500

    Create bear fail.rkt
    fail

---
+++ bear fail.rkt
@@ -0,0 +1,135 @@
+;; constants:
+
+(define WIDTH 400)
+(define HEIGHT 200)
+
+(define CTR-X (/ WIDTH 2))
+(define CTR-Y (/ HEIGHT 2))
+
+(define SPEED 20)
+
+(define BEAR-IMG (square 25 "solid" "brown"))
+
+(define MTS (empty-scene WIDTH HEIGHT "lightblue"))
+
+;; data definitions:
+
+(define-struct bear (s x y))
+;; Bear (make-bear b x y)
+;; interp. the angle and vertical/horizontal positions of bear       
+
+(define B1 (make-bear 0 200 400))
+(define B2 (make-bear 45 12 50))
+
+;(define (fn-for-bear b)
+;  (... (bear-s b)
+;       (bear-x b)
+;       (bear-y b)))
+
+;; ListOfBear is one of:
+;; - empty
+;; - (cons Bear ListOfBear)
+;; - a list of bears
+
+(define LOB1 empty)
+(define LOB2 (cons B1 (cons B2 empty)))
+
+;(define (fn-for-lob lob)
+;  (cond [(empty? lob) (...)]
+;        [else
+;         (fn-for-bear b (bear-s b)
+;         (fn-for-los (rest los)))]))
+
+;; Template rules used:
+;; - atomic distinct: empty
+;; - compund (cons Bear ListOfBear)
+;; - reference rule: (first lob) is Bear
+;; - self-refernce rule: (rest lob) is ListOfBear
+
+
+
+;; Function defintions
+
+; Bear -> Bear
+;; start the world with (main (make-bear 0 CTR-X CTR-Y))
+;; 
+(define (main b)
+  (big-bang b                   
+            (on-tick   add-bear)    
+            (to-draw   render-bears)   
+            (on-mouse  handle-mouse)))
+
+
+;; ListOfString -> ListOfBear
+;; produce an additonal bear on MTS where clicked
+(check-expect (add-bear empty) 
+              empty)
+(check-expect (add-bear (cons (make-bear 0 0 0) empty))
+              (cons (make-bear (+ SPEED 0) 0 0) empty))
+
+;(define (add-bear lob) LOB1) ;stub
+
+(define (add-bear lob)
+  (cond [(empty? lob) 
+         empty]
+        [else
+         (cons (next-bear (first lob))
+               (add-bear (rest lob)))]))
+
+;; Bear -> Bear
+;; create the next angle of bear
+(check-expect (next-bear (make-bear 0 CTR-X CTR-Y)) 
+              (make-bear (+ SPEED   0) CTR-X CTR-Y))
+(check-expect (next-bear (make-bear 200 CTR-X CTR-Y)) 
+              (make-bear (+ SPEED 200) CTR-X CTR-Y))
+
+;(define (next-bear b) (make-bear 0 0 0)) ;stub
+
+(define (next-bear b)
+  (make-bear (+ (bear-s b) SPEED) 
+             (bear-x b) 
+             (bear-y b)))
+
+
+;; ListOfBear -> Image
+;; produce the proper image of Bear on MTS
+(check-expect (render-bears empty) 
+              MTS)
+(check-expect (render-bears (cons (make-bear 20 50 200)
+                                 (cons (make-bear 100 200 300)
+                                       empty)))
+              (place-image (rotate 20 BEAR-IMG) 50 300
+                           (place-image (rotate 100 BEAR-IMG) 200 300
+                                        MTS)))
+
+
+;(define (render-bears lob) MTS) ;stub
+
+(define (render-bears lob)
+  (cond [(empty? lob) MTS]
+        [else
+         (render-bear-on (first lob) (render-bears (rest lob)))]))
+
+;; Bear Image -> Image
+;; render an image of a bear on a given image
+(check-expect (render-bear-on (make-bear 0 0 0) MTS)
+              (place-image (rotate 90 BEAR-IMG) 0 0 MTS)) 
+
+;(define (render-bear-on b img) MTS) ;stub
+
+(define (render-bear-on b img)
+  (place-image (rotate (bear-s b) BEAR-IMG) (bear-x b) (bear-y b) img))
+
+
+;; Bear Integer Integer MouseEvent -> ListOfBear
+;; create a new Bear at current x/y coord of the mouse when clicked
+(check-expect (handle-mouse (make-bear 45 0 0) 20 100 "button-down")
+              (list (make-bear 0 20 100) (make-bear 45 0 0)))
+
+;(define (handle-mouse b x y me) (make-bear 0 0 0))
+
+(define (handle-mouse lob x y me)
+  (cond [(mouse=? me "button-down") 
+         (list (make-bear 0 x y) lob)]
+        [else
+         lob]))
