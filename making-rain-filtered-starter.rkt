4933828d0233690fbb8ad271c93d4f8fd549556f
hughest64 <hughest64@gmail.com>
2016-05-11 13:23:08 -0500

    Update making-rain-filtered-starter.rkt
    done!

--- making-rain-filtered-starter.rkt
+++ making-rain-filtered-starter.rkt
@@ -4,31 +4,6 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
-;; making-rain-filtered-starter.rkt
-
-
-;PROBLEM:
-;
-;Design a simple interactive animation of rain falling down a screen. Wherever we click,
-;a rain drop should be created and as time goes by it should fall. Over time the drops
-;will reach the bottom of the screen and "fall off". You should filter these excess
-;drops out of the world state - otherwise your program is continuing to tick and
-;and draw them long after they are invisible.
-;
-;In your design pay particular attention to the helper rules. In our solution we use
-;these rules to split out helpers:
-;  - function composition
-;  - reference
-;  - knowledge domain shift
-;  
-;  
-;NOTE: This is a fairly long problem.  While you should be getting more comfortable with 
-;world problems there is still a fair amount of work to do here. Our solution has 9
-;functions including main. If you find it is taking you too long then jump ahead to the
-;next homework problem and finish this later.
-
-
-
 ;; Make it rain where we want it to.
 
 ;; =================
@@ -37,7 +12,7 @@
 (define WIDTH  300)
 (define HEIGHT 300)
 
-(define SPEED 1)
+(define SPEED 3)
 
 (define DROP (ellipse 4 8 "solid" "blue"))
 
@@ -173,6 +148,8 @@
 
 (define (render-drops lod)
   (cond [(empty? lod) MTS]
+        [(bottom? (first lod))
+         (render-drops (rest lod))]
         [else
          (render-drop (first lod)
                       (render-drops (rest lod)))]))
